7915b03b77e34cba9e85f05bc3f675cb40f3705b
alexnyebuchan <alexnyebuchan@gmail.com>
2023-12-01 13:12:28 +0000

    footer ammendment 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <p>Web Development terms for practice purposes written by Alex Buchan</p>
+        <p>Web Development terms written by Alex Buchan</p>
         <p>&copy; Alex Buchan 2023</p>
       </div>
     </footer>
