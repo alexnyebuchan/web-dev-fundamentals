4f1e2ab3b2e2bdd5580c406b93a962f09d1b121b
alexnyebuchan <alexnyebuchan@gmail.com>
2023-12-12 22:51:44 +0000

    corrected main test file

--- tests/components/Main.test.ts
+++ tests/components/Main.test.ts
@@ -7,10 +7,8 @@ describe('Main component', () => {
     const button = screen.getByText('Random Term');
     fireEvent.click(button);
 
-    // Add assertions to check if the term is displayed after the button click
     expect(screen.getByText(/title/i)).toBeInTheDocument();
     expect(screen.getByText(/definition/i)).toBeInTheDocument();
   });
 
-  // Add more test cases to cover different scenarios
 });
